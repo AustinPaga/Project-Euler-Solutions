9ac654b820c7bd1ec615389c8c7781cf23192e98
AustinPaga <69488371+AustinPaga@users.noreply.github.com>
2022-06-27 21:42:11 +0900

    inserted problem 3

--- Problems_1-10/Run_Solutions.cs
+++ Problems_1-10/Run_Solutions.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.Reflection;
 
 public class Run_Solutions
 {
@@ -20,6 +17,7 @@ public class Run_Solutions
         {
             case 1: Console.WriteLine(Problem1.SumMultiplesof3and5(1000)); break;
             case 2: Console.WriteLine(Problem2.SumEvenFib(4000000)); break;
+            case 3: Console.WriteLine(Problem3.LargestPrimeFactor(249852836)); break;
             default: break;
         }
     }
